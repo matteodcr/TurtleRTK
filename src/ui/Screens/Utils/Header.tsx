d46225d0434e11a24086d64c8d9a6e976f19bc28
matteodcr <mattdecorsaire@gmail.com>
2023-03-07 15:30:41 +0100

    Rewrite header in Header.tsx

--- src/ui/Screens/Utils/Header.tsx
+++ src/ui/Screens/Utils/Header.tsx
@@ -1,12 +1,16 @@
 import {Pressable, Text, View} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import React from 'react';
+import {styles} from '../CasterScreen/CasterScreen';
 
-const HeaderMoreButton = () => {
-  navigation.navigate('CasterPoolScreen');
-};
+interface HeaderProps {
+  navigation;
+}
 
-const renderHeaderTab = () => {
+export default function Header({navigation}: HeaderProps) {
+  const HeaderButton = () => {
+    navigation.navigate('CasterPoolScreen');
+  };
   return (
     <View style={styles.headerTab}>
       <Text
@@ -18,9 +22,9 @@ const renderHeaderTab = () => {
         }}>
         Caster Screen
       </Text>
-      <Pressable style={styles.TabButton} onPress={HeaderMoreButton}>
+      <Pressable style={styles.TabButton} onPress={HeaderButton}>
         <MaterialIcons name="library-add" color={'white'} size={25} />
       </Pressable>
     </View>
   );
-};
+}
